32d5a8deb85de08b24ea2f3bfe3c700413e3b260
imuzeefansari <imuzeefansari@gmail.com>
2023-06-04 04:59:40 +0500

    Adding all files

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
+
+
 document.getElementById('setBudgetBtn').onclick = function setBudget() {
   document.getElementById('TotalBudgetSpan').innerText = document.getElementById('setBudget').value + " $";
   if(document.getElementById('setBudget').value.length === 0){
     alert('please enter a Budget..');
   }
  }
-
-document.getElementById('push').onclick = function () {
+ document.getElementById('push').onclick = function () {
   if(document.getElementById('input').value.length === 0){
     alert('please enter a Value..');
   }else{
     document.getElementById('tasks').innerHTML += `<div class="task"><span>${document.getElementById('categories').value}</span><span id="taskname">${document.getElementById('input').value}</span><span>${document.getElementById('amount').value}</span><span>${document.getElementById('date').value}</span><button class="delete">X</button></div>`;
   };
+
+
 
 let userAmount = document.getElementById('amount');
 let expensesSpan = document.getElementById('expenses');
