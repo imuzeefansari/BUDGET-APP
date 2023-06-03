334f932436e77da3226b84651bf25c80b4c3de94
imuzeefansari <imuzeefansari@gmail.com>
2023-06-03 20:47:32 +0500

    modified all files

--- project.js
+++ project.js
@@ -1,40 +1,38 @@
+document.getElementById('setBudgetBtn').onclick = function setBudget() {
+  document.getElementById('TotalBudgetSpan').innerText = document.getElementById('setBudget').value + " $";
+  if(document.getElementById('setBudget').value.length === 0){
+    alert('please enter a Budget..');
+  }
+ }
+
 document.getElementById('push').onclick = function () {
   if(document.getElementById('input').value.length === 0){
-    alert('please enter a task');
+    alert('please enter a Value..');
   }else{
-    document.getElementById('tasks').innerHTML += `<div class="task"><span id="taskname">${document.getElementById('input').value}</span>
-    <span>${document.getElementById('amount').value}</span>
-    <button class="delete">X</button>
-</div>`;
-  }
+    document.getElementById('tasks').innerHTML += `<div class="task"><span>${document.getElementById('categories').value}</span><span id="taskname">${document.getElementById('input').value}</span><span>${document.getElementById('amount').value}</span><span>${document.getElementById('date').value}</span><button class="delete">X</button></div>`;
+  };
+
 let userAmount = document.getElementById('amount');
 let expensesSpan = document.getElementById('expenses');
-let expenditure = parseInt(userAmount.value);
-let sum = parseInt(expensesSpan.innerText) + expenditure;
-let totalBudget = document.getElementById('setBudget').value;
-let remainingBal = totalBudget - sum;
-document.getElementById('remBal').innerText = remainingBal;
+let expenditure = parseFloat(userAmount.value);
+let previousExp = parseFloat(expensesSpan.innerText);
+let nexExp = previousExp + expenditure;
+expensesSpan.textContent = nexExp.toFixed(2) + " $";
+
+
+let totalBudget = document.getElementById('setBudget');
+let remainingBalSpan = document.getElementById('remBal');
+let totalBudgetFloat = parseFloat(totalBudget.value);
+let remainingBal = totalBudgetFloat - nexExp;
+remainingBalSpan.textContent = remainingBal.toFixed(2) + " $";
+
 
 let trash = document.getElementsByClassName('delete');
 for (let i = 0; i < trash.length; i++) {
   trash[i].onclick = function () {
-   this.parentNode.remove();
+  this.parentNode.remove();
   }
-}
-var span = document.getElementById('taskname');
-for (let i = 0; i < span.length; i++) {
-  span[i].onclick = function () {
-    this.classList.toggle('completed');
-  };
 }
 document.getElementById('input').value = '';
 document.getElementById('amount').value = '';
-
-
-  }
-  document.getElementById('setBudgetBtn').onclick = function setBudget() {
-   let setBudget =  document.getElementById('TotalBudgetSpan').innerText = document.getElementById('setBudget').value;
-   document.getElementById('setBudget').value = '';
-  }
-  
-  
+}
